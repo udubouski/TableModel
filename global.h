b76c35f61a1b129f87a9febdde08960fb0eb7a73
Vladislav <dub4ik06@gmail.com>
2017-11-03 00:02:00 +0300

    add files to model

--- global.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-const int MinCode = 0;
-const int MaxCode = 99999;
-const int InvalidCode = -1;
-enum Column {Student, Father, MoneyFather, Mother, MoneyMother, NumberBrothers, NumberSisters};
-#endif // GLOBAL_H
